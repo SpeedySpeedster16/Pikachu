5599342ffcd6780f0f1cb9aabbdc6e0a9212dc3d
The Legend <75024331+thelegend-16@users.noreply.github.com>
2021-02-04 10:19:44 +0530

    Update fun.py

--- SaitamaRobot/modules/fun.py
+++ SaitamaRobot/modules/fun.py
@@ -11,7 +11,7 @@ from telegram import ChatPermissions, ParseMode, Update
 from telegram.error import BadRequest
 from telegram.ext import CallbackContext, run_async
 
-GIF_ID = 'CgACAgQAAx0CSVUvGgAC7KpfWxMrgGyQs-GUUJgt-TSO8cOIDgACaAgAAlZD0VHT3Zynpr5nGxsE'
+GIF_ID = 'https://telegra.ph/file/c19ac7ce1f7c40c680ef1.mp4'
 
 
 @run_async
